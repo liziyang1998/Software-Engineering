60c9a5a5cc659a25d266dd9ca02f33f2943d0709
liziyang1998 <lzy1998@mail.ustc.edu.cn>
2019-03-26 22:24:08 +0800

    upgrade

--- Lab1/Word Chain.cpp
+++ Lab1/Word Chain.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdio>
 #include "maxWordLength.h"
+#define Debug1
 
 using namespace std;
 FILE * fin;
@@ -10,17 +11,19 @@ int main(int argc, char* argv[]){
         fin = fopen(argv[2], "r");
         max_Word_Length word_list;
         word_list.Init(fin);
-        //word_list.Print();
-        if (argv[1][1] == 'w'){
+#ifdef Debug
+        word_list.Print();
+#endif
+        if (argv[1][1] == 'w'){//最长单词链
             word_list.makeEdge();
             word_list.maxWord();
         }
-        else if (argv[1][1] == 'c'){
+        else if (argv[1][1] == 'c'){//最多字母数目
             word_list.makeEdge();
             word_list.maxLength();
         }
     }
-    else if(argv[1][1] == 'n'){
+    else if(argv[1][1] == 'n'){//指定单词数
         fin = fopen(argv[4], "r");
         max_Word_Length word_list;
         word_list.Init(fin);
@@ -28,21 +31,21 @@ int main(int argc, char* argv[]){
         word_list.wordLength(argv[2][0]);
     }
     else {
-        if(argc > 5){//head and tail
+        if(argc > 5){//指定头和尾
             fin = fopen(argv[6], "r");
             max_Word_Length word_list;
             word_list.Init(fin);
             word_list.makeEdge();
             word_list.maxWordHeadAndTail(argv[2][0], argv[4][0]);
         }
-        else if(argv[1][1] == 'h'){//head
+        else if(argv[1][1] == 'h'){//指定头
             fin = fopen(argv[4], "r");
             max_Word_Length word_list;
             word_list.Init(fin);
             word_list.makeEdge();
             word_list.maxWordHead(argv[2][0]);
         }
-        else {//tail
+        else {//指定尾
             fin = fopen(argv[4], "r");
             max_Word_Length word_list;
             word_list.Init(fin);
@@ -50,4 +53,5 @@ int main(int argc, char* argv[]){
             word_list.maxWordTail(argv[2][0]);
         }
     }
+    return 0;
 }
